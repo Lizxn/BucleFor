b8ec4ec58047b2d4b34d0e5a8038233647618fd2
Lizxn <lisa.studio39@gmail.com>
2025-05-07 08:21:50 -0500

    Add files via upload

---
+++ Bucleforpinedagomezallison.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.bucleforpinedagomezallison;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Bucleforpinedagomezallison {
+
+    public static void main(String[] args) {
+        int valores[] = {18, 28, 38, 48, 58, 68,78};
+        
+        for (int i=2; i <= valores.length; i++){
+        System.out.println(valores[1]);
+    }
+   }
+}
